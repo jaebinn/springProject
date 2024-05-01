72d1f172b1453f67993d6d315d3e6f67cefb5318
simhanju <hanju0007@naver.com>
2024-05-01 12:44:41 +0900

    커밋

--- test/src/main/java/com/example/demo/Test_SHJ.java
+++ test/src/main/java/com/example/demo/Test_SHJ.java
@@ -2,11 +2,9 @@ package com.example.demo;
 
 public class Test_SHJ {
 	private void test() {
-
 		System.out.println("gkdldkdld");
-		System.out.println("중리가쟈");
-		System.out.println("중리가쟈 안갈껴??");
-		System.out.println("심한주 simhaju");
+		
+
 	
 	}
 }
