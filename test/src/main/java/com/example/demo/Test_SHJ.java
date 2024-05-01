29f6b5e901c7071cd52792738d65f9bcafa56151
mococo <ash929@naver.com>
2024-05-01 12:30:41 +0900

    오점뭐

--- test/src/main/java/com/example/demo/Test_SHJ.java
+++ test/src/main/java/com/example/demo/Test_SHJ.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class Test_SHJ {
 	private void test() {
-		System.out.println("simhanju");
+		System.out.println("배고프다");
 		System.out.println("한줄더 추가하기");
 		System.out.println("한줄더 더 추가하기");
 		System.out.println("한줄더 더 추가하기");
