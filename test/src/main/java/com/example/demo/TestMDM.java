7a8bee4d37b329c3e0c67a7febcd4b6ff9a17401
MinDongmin <mdmin1004@naver.com>
2024-05-01 11:55:38 +0900

    MDM 다시 수정

--- test/src/main/java/com/example/demo/TestMDM.java
+++ test/src/main/java/com/example/demo/TestMDM.java
@@ -3,6 +3,6 @@ package com.example.demo;
 public class TestMDM {
 	public void MDMTest() {
 		System.out.println("커밋 테스트!!!");
-		System.out.println("커밋 테스트2");
+		System.out.println("커밋 테스트2 이걸 바꾸면 같은 부분이 수정됬다고 나오나?");
 	}
 }
