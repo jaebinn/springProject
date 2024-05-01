02860f5737688dd6c7ae6e65a51f547ef1547a74
mococo <ash929@naver.com>
2024-05-01 12:25:25 +0900

    Merge branch 'main' of https://github.com/jaebinn/springProject.git into main

--- test/src/main/java/com/example/demo/Test_SHJ.java
+++ test/src/main/java/com/example/demo/Test_SHJ.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 public class Test_SHJ {
 	private void test() {
-		System.out.println("Simhanjuclass 테스트");
+		System.out.println("심한주 테스트");
 		System.out.println("한줄더 추가하기");
+		System.out.println("한줄더 더 추가하기");
+		System.out.println("한줄더 더 추가하기");
+		System.out.println("한줄더 더 추가하기");
+		System.out.println("한줄더 더 추가하기");
 		System.out.println("한줄더 더 추가하기");
 	}
 }
