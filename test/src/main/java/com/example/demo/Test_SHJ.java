6ced0d6b4aeed6a1c516c426d4cd1d83003cab61
mococo <ash929@naver.com>
2024-05-01 12:34:30 +0900

    중리

--- test/src/main/java/com/example/demo/Test_SHJ.java
+++ test/src/main/java/com/example/demo/Test_SHJ.java
@@ -2,7 +2,8 @@ package com.example.demo;
 
 public class Test_SHJ {
 	private void test() {
-		System.out.println("배고프다");
+		System.out.println("gkdldkdld");
+		System.out.println("중리가쟈");
 		System.out.println("한줄더 추가하기");
 		System.out.println("한줄더 더 추가하기");
 		System.out.println("한줄더 더 추가하기");
