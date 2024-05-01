b491e338b90edb2dd74e95bbd5c23d5ec6755a0c
simhanju <hanju0007@naver.com>
2024-05-01 12:24:08 +0900

    원래 코드 다른 코드로 변경 테스트

--- test/src/main/java/com/example/demo/Test_SHJ.java
+++ test/src/main/java/com/example/demo/Test_SHJ.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class Test_SHJ {
 	private void test() {
-		System.out.println("심한주class 테스트");
+		System.out.println("Simhanjuclass 테스트");
 		System.out.println("한줄더 추가하기");
 		System.out.println("한줄더 더 추가하기");
 	}
