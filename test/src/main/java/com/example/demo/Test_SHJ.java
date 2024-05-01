9b0812202ebf463f273277c947fa18190a066e97
simhanju <hanju0007@naver.com>
2024-05-01 12:28:32 +0900

    영어로 바꿈

--- test/src/main/java/com/example/demo/Test_SHJ.java
+++ test/src/main/java/com/example/demo/Test_SHJ.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class Test_SHJ {
 	private void test() {
-		System.out.println("심한주 는 바보다");
+		System.out.println("simhanju");
 		System.out.println("한줄더 추가하기");
 		System.out.println("한줄더 더 추가하기");
 		System.out.println("한줄더 더 추가하기");
