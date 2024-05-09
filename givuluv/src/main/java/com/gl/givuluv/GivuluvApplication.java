40caef4c207a3933eb2c359a195d72050a5ae31e
jaebinn <whiteclover129@gmail.com>
2024-05-09 17:34:30 +0900

    시작...

--- givuluv/src/main/java/com/gl/givuluv/GivuluvApplication.java
+++ givuluv/src/main/java/com/gl/givuluv/GivuluvApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.gl.givuluv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestTestApplication {
+public class GivuluvApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-		System.out.println("hello world");
+		SpringApplication.run(GivuluvApplication.class, args);
 	}
+
 }
